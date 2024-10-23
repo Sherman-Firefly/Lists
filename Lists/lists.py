3854d2bfeded2a93632a04c3f5f7b2ce3b229b99
Sherman-Firefly <efracw2007@gmail.com>
2024-10-23 17:18:54 +0800

    lists

---
+++ Lists/lists.py
@@ -0,0 +1,15 @@
+empty=[]
+print(empty)
+
+num=[1,2,3,4,5,6,7,8,9,10]
+print(num)
+
+list1=[2,33.1,"Hi there", 2001]
+print(list1)
+
+mul=[1,2,3,4,5]*3
+print(mul)
+
+list2=[10,20,30,40,50,60,70]
+list2=list2[-1::]
+print(list2)
